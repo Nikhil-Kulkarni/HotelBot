997d55a30ff2f0cd7c1ccbeb91b879322e3389ce
Nikhil Kulkarni <nikhil896@gmail.com>
2018-11-29 20:43:18 -0800

    first messenger tutorial

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express().use(bodyParser.json());
+
+// Messenger platform sends all webhook events to this post endpoint
+app.post('/webhook', (req, res) => {
+    let body = req.body;
+    
+    if (body.object === 'page') {
+        body.entry.forEach(entry => {
+            let webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404)
+    }
+});
+
+// Messenger platform verifies webook via this get endpoints
+app.get('/webhook', (req, res) => {
+    let verifyToken = "nikhil";
+
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === verifyToken) {
+            console.log('webhook verified');
+            res.status(200).send(challenge);
+        }  else {
+            res.sendStatus(403); 
+        }
+    } else {
+        res.sendStatus(403); 
+    }
+});
+
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
