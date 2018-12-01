0b720480938a1bda963d2db6befef5fb29590855
Nikhil Kulkarni <nikhil896@gmail.com>
2018-11-30 22:29:29 -0800

    typing indicator

--- send.js
+++ send.js
@@ -2,6 +2,33 @@ const messages = require('./messages');
 const api = require('./api');
 const _ = require('lodash');
 
+const typingOn = (recipientId) => {
+    return {
+        "recipient": {
+            "id": recipientId,
+        },
+        "sender_action": "typing_on"
+    }
+}
+
+const typingOff = (recipientId) => {
+    return {
+        "recipient": {
+            "id": recipientId,
+        },
+        "sender_action": "typing_off"
+    }
+}
+
+const markSeen = (recipientId) => {
+    return {
+        "recipient": {
+            "id": recipientId,
+        },
+        "sender_action": "mark_seen"
+    }
+}
+
 const messageToJson = (recipientId, messagePayload) => {
     return {
         recipient: {
@@ -15,7 +42,9 @@ const sendMessage = (recipientId, messagePayloads) => {
     const messagePayloadArray = _.castArray(messagePayloads)
         .map((payload) => messageToJson(recipientId, payload));
 
-    api.callMessagesApi(messagePayloadArray);
+    api.callMessagesApi([
+        typingOn(recipientId),
+        ...messagePayloadArray]);
 };
 
 const sendDummyMessage = (recipientId) => {
