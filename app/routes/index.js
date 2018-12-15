526cb24f023a502b4fe25bcd312f825b6f88385b
Nikhil Kulkarni <nikhil896@gmail.com>
2018-12-15 15:16:26 -0800

    nit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,4 @@
 const app = module.exports = require('express')();
 
 app.use('/webhook', require('./messenger'));
-app.use('/reservation', require('./reservations'));
 app.use('/api', require('./graphql'));
--- app/routes/reservations.js
+++
@@ -1,6 +0,0 @@
-const app = module.exports = require('express')();
-
-
-app.get('/test', (req, res) => {
-    res.status(200).send('test success')
-});
