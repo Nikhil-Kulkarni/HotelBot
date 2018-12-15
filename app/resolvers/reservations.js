d0baf7d7aa9a404a470101ff3cb222239f6b1dee
Nikhil Kulkarni <nikhil896@gmail.com>
2018-12-15 13:54:43 -0800

    nit

--- app/resolvers/reservations.js
+++ app/resolvers/reservations.js
@@ -10,12 +10,5 @@ const reservationsResolver = {
 };
 
 module.exports = {
-    resolver: {
-        Query: {
-            reservations(_, args, ctx) {
-                const query = 'select id, hotel_id, phone_number, name, room_number, checked_in from reservations';
-                return db.manyOrNone(query);
-            }
-        }
-    }
+    resolver: reservationsResolver
 };
--- app/routes/graphql.js
+++ app/routes/graphql.js
@@ -2,11 +2,16 @@ const app = require('express')();
 const graphqlExpress = require('graphql-server-express').graphqlExpress;
 const graphiqlExpress = require('graphql-server-express').graphiqlExpress;
 const schema = require('../schemas/reservations').schema;
+const db = require('../actions/dbclient').db;
 
 app.use('/graphiql', graphiqlExpress({
     endpointURL: '/api',
 }));
 
+app.get('/testdb', (req, res) => {
+    const rows = db.manyOrNone('select * from reservations');
+    res.status(200).send(rows);
+});
 app.use('/', graphqlExpress({ schema }));
 
 module.exports = app;
