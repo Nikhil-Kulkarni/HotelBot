26299c9ef78e456e90ac8be35ffcc3f43223fcff
Nikhil Kulkarni <Nikhil896@gmail.com>
2018-12-15 15:18:22 -0800

    Use GraphQL & pg-promise (#1)
    * graphql
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * nit
    
    * wip
    
    * nit
    
    * nit
    
    * wip
    
    * nit

--- app/routes/reservations.js
+++
@@ -1,6 +0,0 @@
-const app = module.exports = require('express')();
-const queries = require('../actions/queries');
-
-app.get('/test', (req, res) => {
-    res.status(200).send(queries.fetchReservation('messenger', '+14048600194'));
-});
---
+++ app/schemas/reservations.js
@@ -0,0 +1,25 @@
+const GraphQLSchema = require('graphql').GraphQLSchema;
+const makeExecutableSchema = require('graphql-tools').makeExecutableSchema;
+const resolvers = require('../resolvers/reservations').resolvers;
+
+const typeDefs = `
+    type Reservation {
+        id: String!
+        hotel_id: String
+        phone_number: String
+        name: String
+        room_number: Int
+        checked_in: Boolean
+    }
+
+    type Query {
+        reservations: [Reservation]
+    }
+`;
+
+module.exports =  {
+    schema: makeExecutableSchema({
+        typeDefs, 
+        resolvers
+    })
+};
