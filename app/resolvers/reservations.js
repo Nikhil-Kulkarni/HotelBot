65fe60d6b19f18392113f2d0d12e1d47c7c80a91
Nikhil Kulkarni <nikhil896@gmail.com>
2018-12-15 10:16:53 -0800

    nit

--- app/resolvers/reservations.js
+++ app/resolvers/reservations.js
@@ -4,13 +4,18 @@ const reservationsResolver = {
     Query: {
         reservations(_, args, ctx) {
             const query = 'select id, hotel_id, phone_number, name, room_number, checked_in from reservations';
-            console.log(query);
-            console.log(db.manyOrNone(query));
             return db.manyOrNone(query);
         }
     }
 };
 
 module.exports = {
-    resolver: reservationsResolver
+    resolver: {
+        Query: {
+            reservations(_, args, ctx) {
+                const query = 'select id, hotel_id, phone_number, name, room_number, checked_in from reservations';
+                return db.manyOrNone(query);
+            }
+        }
+    }
 };
